6c07a72ed774b429949410db8d3120a23bd00451
ibrahimmusa173 <musaaman705@gmail.com>
2025-05-13 08:21:40 -0700

    ok

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+
+import Mobile from './Mobile'
+import Desktop from './Desktop'
+import WhatsApp from './Whatsapp'
+
+function Navbar() {
+  return (
+    <>
+      <Mobile />
+      <Desktop />
+      <WhatsApp />
+    </>
+  )
+}
+
+export default Navbar
