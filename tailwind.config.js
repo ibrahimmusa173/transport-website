068bf68e29dc455e2ac970f289e66bf840d6fbf2
ibrahimmusa173 <musaaman705@gmail.com>
2025-06-02 06:35:21 -0700

    Revert "ok"
    This reverts commit 0e369a4402f266602dd2d123f72c713feede8f49.

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,7 +19,8 @@ export default {
 
 // /** @type {import('tailwindcss').Config} */
 // module.exports = {
-  content: [    "./src/**/*.{js,jsx,ts,tsx}",
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {
