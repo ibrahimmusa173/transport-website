0e369a4402f266602dd2d123f72c713feede8f49
ibrahimmusa173 <musaaman705@gmail.com>
2025-06-02 06:35:13 -0700

    ok

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,8 +19,7 @@ export default {
 
 // /** @type {import('tailwindcss').Config} */
 // module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+  content: [    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {
